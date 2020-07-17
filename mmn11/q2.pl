fac22cffeee727294a09f38918bb8b693e424783
Ron Kantor <ron.kantor@theculturetrip.com>
2020-07-17 13:04:27 +0300

    finish mmn11

--- mmn11/q2.pl
+++ mmn11/q2.pl
@@ -2,35 +2,41 @@ found_on(bert,blood).
 found_on(alan,mud).
 found_on(jon,choclate).
 found_on(barbara,choclate).
-found_on(suzan,blood).
 found_on(suzan,pudra).
+found_on(suzan,blood).
 
 have(bert,wooden_leg).
 have(jon,pistol).
 have(alan,rod).
 have(barbara,scissors).
 
-works_like(wooden_leg,club).
-works_like(rod,club).
-works_like(scissors,knife).
+similar(wooden_leg,club).
+similar(rod,club).
+similar(scissors,knife).
 
 romance(jon,barbara).
 romance(bert,barbara).
 romance(jon,suzan).
 
-murdered(susan,club).
+murdered(suzan,club).
 
 
 
 killer(Person):-
-    suspect(Person),
+    found_on(Victim,X),
+    found_on(Person,X),
     murdered(Victim,_),
-    found_on(Person,Material),
-    found_on(Victim,Material).
+    suspect(Person).
+
 
 
 
 % Helper relations
+
+works_like(I1,I2):-
+    similar(I1,I2)
+    ;
+    similar(I2,I1).
 
 have_romance(P1,P2):-
     romance(P1,P2)
@@ -39,16 +45,12 @@ have_romance(P1,P2):-
 
 suspect(Person):-
     % if have an item similar to to the killing item
-    have(Person,Item),
-    works_like(Item,KillingItem),
-    murdered(_,KillingItem)
-    ; % if killed with the same item
-    have(Person,Item),
-    murdered(_,KillingItem),
-    Item = KillingItem
+    have(Person,X),
+    works_like(X,Item),
+    murdered(_,Item)
     ; % if have romance
-    murdered(Victim,_),
-    have_romance(Person,Victim)
+    have_romance(Person,Victim),
+    murdered(Victim,_)
     ; % if have romance with someone who is in a romance with a victim
     murdered(Victim,_),
-    have_romance(Person,X), have_romance(X,Victim), X\=Victim.
+    have_romance(Person,X), have_romance(X,Victim), Victim\=X.
